183655e150b605ceca145088390207b65229e9ea
phrrezende <phr.rezende@gmail.com>
2015-05-11 15:11:40 -0300

    Configuração de Acesso ao BD
    Alteração do arquivo responsável pelo acesso ao banco de dados na nuvem
    azure.

--- config/config_bd.php
+++ config/config_bd.php
@@ -1,10 +1,10 @@
 ﻿<?php 
 	function conn_mysql(){
-		$servidor= "localhost";
+		$servidor= "br-cdbr-azure-south-a.cloudapp.net";
 		$porta=3306;
-		$banco="daw_yearbook";
-		$usuario="daw";
-		$senha="daw2014";
+		$banco="nuvemwoAxTH9bDmK";
+		$usuario="bc45853ca2d1a6";
+		$senha="c43a9608";
 		
 		$conn = new PDO("mysql:host=$servidor;
 		port=$porta;
