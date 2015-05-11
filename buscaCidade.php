2ac9474f3ab513774e945ff3d91b91e145acc004
phrrezende <phr.rezende@gmail.com>
2015-05-11 16:37:36 -0300

    Primeiro Commit
    Script para buscar cidades por estado

---
+++ buscaCidade.php
@@ -0,0 +1,21 @@
+﻿<?php 
+require("./config/config_bd.php");
+
+$idEstado=$_POST['estado'];
+$conexao= conn_mysql();
+$sqlCidades="select * from cidades where idEstado=?";
+$operacao=$conexao->prepare($sqlCidades);
+$operacao->execute(array($idEstado));
+$cidades=$operacao->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<?php 
+foreach($cidades as $cidade){
+	$cidade['nomeCidade']=utf8_encode($cidade['nomeCidade']);
+	
+	echo "<option value='$cidade[idCidade]'>$cidade[nomeCidade]</option>";
+}
+
+?>
+
